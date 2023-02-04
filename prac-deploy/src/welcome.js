13a822d5a1526487d0c2537c5fd56a06e17c4c9f
Cindy Kim <114192884+CindyKim95@users.noreply.github.com>
2023-02-04 01:12:04 -0600

    first commit

---
+++ prac-deploy/src/welcome.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Title from "./title";
+
+export default class Welcome extends React.Component {
+    render() {
+        return (
+            <div>
+            <Title />
+            <h2>Welcome to my page</h2>
+        </div>
+        ) 
+    }
+}
